b0a2978ddff099dbdc63eba977af938d3a9696f1
rg3915 <rg3915@yahoo.com.br>
2015-12-23 04:20:15 -0200

    tests for subscriptions

---
+++ eventex/subscriptions/tests.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from eventex.subscriptions.forms import SubscriptionForm
+
+
+class SubscribeTest(TestCase):
+
+    def setUp(self):
+        self.resp = self.client.get('/inscricao/')
+
+    def test_get(self):
+        ''' GET /inscricao/ must return status code 200 '''
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        ''' Must use subscriptions/subscription_form.html '''
+        self.assertTemplateUsed(
+            self.resp, 'subscriptions/subscription_form.html')
+
+    def test_html(self):
+        ''' Html must contain input tags '''
+        self.assertContains(self.resp, '<form')
+        self.assertContains(self.resp, '<input', 6)
+        self.assertContains(self.resp, 'type="text"', 3)
+        self.assertContains(self.resp, 'type="email"')
+        self.assertContains(self.resp, 'type="submit"')
+
+    def test_csrf(self):
+        ''' Html must contain csrf '''
+        self.assertContains(self.resp, 'csrfmiddlewaretoken')
+
+    def test_has_form(self):
+        ''' Context must have subscription form '''
+        form = self.resp.context['form']
+        self.assertIsInstance(form, SubscriptionForm)
+
+    def test_form_has_fields(self):
+        ''' Form must have 4 fields '''
+        form = self.resp.context['form']
+        self.assertSequenceEqual(
+            ['name', 'cpf', 'email', 'phone'], list(form.fields))
