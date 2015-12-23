b529fe24c13fcb342371506736a1df11600da3ab
rg3915 <rg3915@yahoo.com.br>
2015-12-23 04:21:01 -0200

    subscription form hardcoded

---
+++ eventex/subscriptions/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class SubscriptionForm(forms.Form):
+    name = forms.CharField()
+    cpf = forms.CharField()
+    email = forms.EmailField()
+    phone = forms.CharField()
---
+++ eventex/subscriptions/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from eventex.subscriptions.forms import SubscriptionForm
+
+
+def subscribe(request):
+    context = {'form': SubscriptionForm()}
+    return render(request, 'subscriptions/subscription_form.html', context)
