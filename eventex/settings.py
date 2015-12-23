41fc3f9eda6294ba752e7702b1760beee33832ac
rg3915 <rg3915@yahoo.com.br>
2015-12-23 04:19:43 -0200

    App subscriptions

--- eventex/settings.py
+++ eventex/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'eventex.core',
+    'eventex.subscriptions',
 ]
 
 MIDDLEWARE_CLASSES = [
--- eventex/urls.py
+++ eventex/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url
 from django.contrib import admin
 import eventex.core.views as v
+import eventex.subscriptions.views as s
 
 urlpatterns = [
     url(r'^$', v.home, name='home'),
     url(r'^email/$', v.sendemail, name='sendemail'),
     url(r'^email/subscribe$', v.subscribe, name='subscribe'),
+
+    url(r'^inscricao/$', s.subscribe, name='subscribe'),
+
     url(r'^admin/', admin.site.urls),
 ]
