d51b1a050c94c93ee3053d66517cad88718ff0b3
rg3915 <rg3915@yahoo.com.br>
2015-12-27 01:12:37 -0200

    Habilitado email

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -13,3 +13,6 @@ class HomeTest(TestCase):
     def test_template(self):
         ''' Must use index.html '''
         self.assertTemplateUsed(self.response, 'index.html')
+
+    def test_subscription_link(self):
+        self.assertContains(self.response, 'href="/inscricao/"')
