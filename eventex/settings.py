c744b3ceb9f9fb48557fe371e3cf72c576cebd0a
rg3915 <rg3915@yahoo.com.br>
2016-01-08 11:06:33 -0200

    hotfix settings

--- eventex/settings.py
+++ eventex/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'eventex.wsgi.application'
 
 default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 DATABASES = {
-    'default': config('DATABA_URL', default=default_dburl, cast=dburl),
+    'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
 
 
