844356e6cace761fe3d9bc9e672ab3c04699ebd5
rg3915 <rg3915@yahoo.com.br>
2016-01-21 00:44:59 -0200

    Refactor selenium_subscription.py

--- eventex/subscriptions/tests/selenium/selenium_subscription.py
+++ eventex/subscriptions/tests/selenium/selenium_subscription.py
@@ -3,7 +3,7 @@ import random
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from gen_random_values import gen_doc, gen_phone
+from gen_random_values import gen_cpf, gen_phone
 
 
 first_name = names.get_first_name()
@@ -17,24 +17,17 @@ page = webdriver.Firefox()
 # page = webdriver.Chrome(executable_path='/home/rg3915/Downloads/chromedriver')
 page.get('http://localhost:8000/inscricao/')
 
+fields = [['id_name', name],
+          ['id_cpf', gen_cpf()],
+          ['id_email', email],
+          ['id_phone', gen_phone()]]
 
-search = page.find_element_by_id('id_name')
-search.send_keys(name)
-# time.sleep(1)
-
-search = page.find_element_by_id('id_cpf')
-search.send_keys(gen_doc())
-# time.sleep(1)
-
-search = page.find_element_by_id('id_email')
-search.send_keys(email)
-# time.sleep(1)
-
-search = page.find_element_by_id('id_phone')
-search.send_keys(gen_phone())
-# time.sleep(2)
+for field in fields:
+    search = page.find_element_by_id(field[0])
+    search.send_keys(field[1])
+    # time.sleep(0.5)
 
 button = page.find_element_by_id('id_submit')
 button.click()
 
-# page.quit()
+page.quit()
