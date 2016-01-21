79bae86771849e62758d6abfb00a0d475ef21d19
rg3915 <rg3915@yahoo.com.br>
2016-01-21 01:55:43 -0200

    Keynote Speakers

--- eventex/core/tests/test_view_home.py
+++ eventex/core/tests/test_view_home.py
@@ -18,3 +18,19 @@ class HomeTest(TestCase):
     def test_subscription_link(self):
         expected = 'href="{}"'.format(r('subscriptions:new'))
         self.assertContains(self.response, expected)
+
+    def test_speakers(self):
+        ''' Must show keynote speakers '''
+        contents = [
+            'Grace Hopper',
+            'http://hbn.link/hopper-pic',
+            'Alan Turing',
+            'http://hbn.link/turing-pic',
+        ]
+        for expected in contents:
+            with self.subTest():
+                self.assertContains(self.response, expected)
+
+    def test_speakers_link(self):
+        expected = 'href="{}#speakers"'.format(r('core:home'))
+        self.assertContains(self.response, expected)
--- eventex/core/views.py
+++ eventex/core/views.py
@@ -4,7 +4,11 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 
 def home(request):
-    return render(request, 'index.html')
+    speakers = [
+        {'name': 'Grace Hopper', 'photo': 'http://hbn.link/hopper-pic'},
+        {'name': 'Alan Turing', 'photo': 'http://hbn.link/turing-pic'},
+    ]
+    return render(request, 'index.html', {'speakers': speakers})
 
 
 def bubble(request):
