ea49fd2e7f71f9328767e41764b24a3d14b03121
rg3915 <regis42santos@gmail.com>
2021-03-04 04:33:40 -0300

    Fix env_gen.py

--- contrib/env_gen.py
+++ contrib/env_gen.py
@@ -14,12 +14,12 @@ SECRET_KEY=%s
 ALLOWED_HOSTS=127.0.0.1, .localhost
 #DATABASE_URL=postgres://USER:PASSWORD@HOST:PORT/NAME
 #DEFAULT_FROM_EMAIL=
-#EMAIL_BACKEND=django.core.mail.backends.smtp.EmailBackend
-#EMAIL_HOST=
-#EMAIL_PORT=
-#EMAIL_USE_TLS=
-#EMAIL_HOST_USER=
-#EMAIL_HOST_PASSWORD=
+EMAIL_BACKEND=django.core.mail.backends.console.EmailBackend
+EMAIL_HOST=localhost
+EMAIL_PORT=25
+EMAIL_USE_TLS=False
+EMAIL_HOST_USER=
+EMAIL_HOST_PASSWORD=
 """.strip() % get_random_string(50, chars)
 
 # Writing our configuration file to '.env'
