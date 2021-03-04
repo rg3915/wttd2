68c4f723f5eea2802209862d323825f33a445154
rg3915 <regis42santos@gmail.com>
2021-03-04 04:33:48 -0300

    Fix url id to pk.

--- eventex/subscriptions/urls.py
+++ eventex/subscriptions/urls.py
@@ -7,7 +7,7 @@ app_name = 'subscriptions'
 
 urlpatterns = [
     path('', s.new, name='new'),
-    path('<int:id>/', s.detail, name='detail'),
+    path('<int:pk>/', s.detail, name='detail'),
     path('json/donut/', s.paid_list_json, name='paid_list_json'),
     path('json/column/', s.paid_column_json, name='paid_column_json'),
     path('graphic/', s.graphic, name='graphic'),
